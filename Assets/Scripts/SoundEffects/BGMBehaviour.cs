8fc147dedfc37a128782514da6d77a6a4509b384
小小的大肥 <11911839@mail.sustech.edu.cn>
2023-11-29 22:09:20 -0500

    webgl build

---
+++ Assets/Scripts/SoundEffects/BGMBehaviour.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+namespace SoundEffects
+{
+    public class BGMBehaviour : MonoBehaviour
+    {
+        private static BGMBehaviour _instance;
+
+        void Awake()
+        {
+            if (_instance == null)
+            {
+                _instance = this;
+                DontDestroyOnLoad(gameObject);
+            }
+            else
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+}
